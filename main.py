d05fc38fb30f7b674211d65c41f8fefa6c99f01a
Saif Mahmud <67528956+mridul-1999@users.noreply.github.com>
2023-04-06 09:48:52 +0000

    Day_5

---
+++ main.py
@@ -0,0 +1,37 @@
+# Day-5 challange
+print("Marvel Movie Cahracter Creator")
+print()
+around = input("Do you like 'hanging around'?: ")
+if around == 'No':
+  print("Then you're not Spider-man")
+print()
+voice = input("Do you have a 'Gravelly' voice?: ")
+if voice == 'No':
+  print("Aww, then you're not Korg")
+print()
+feel = input("Do you often fell 'Marvelous'?: ")
+if feel =='Yes':
+  print("Aha! You're Captain Marvel! Hi!")
+print()
+
+strong = input("Are you super strong?: ")
+if strong == 'Yes':
+  print("You have got to be Thor!")
+else:
+  print("I guess you are not like anyone from 'Avengers.'")
+
+
+
+
+
+
+
+'''
+# fix my code
+
+drink = input("Do you prefer coffee or tea? ")
+if drink == "coffee":
+  print("Tea is better.")
+else:
+  print("Excellent choice.")
+  '''
